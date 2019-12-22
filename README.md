579a9e1d0df24a142f8f51877e3264a6092b742d
qinyou <qinyou1994@outlook.com>
2019-12-22 20:42:32 +0800

    edit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-运行后 体验地址： http://localhost:8888/v1/index.html 
-Api 地址 http://localhost:8888/v1/swagger-ui.html
+<p align="center">`api-server` 前后分离 权限管理 后台脚手架</p>
+
+## 运行
+``` bash
+1. IDEA clone 本项目
+2. 将api_server.sql 导入 mysql, 配置application-dev.yml 中数据库连接信息
+3. 运行 ApiServerApplication
+```
+预览地址 [http://localhost:8888/v1/index.html](http://localhost:8888/v1/index.html)
+Swagger Api 地址 [http://localhost:8888/v1/swagger-ui.html](http://localhost:8888/v1/swagger-ui.html)
+
+## 截图
+![个人](./预览/profile.png)
+![资源](./预览/resource.png)
+![角色](./预览/role.png)
+![角色配资源](./预览/roleConfigResources.png)
+![用户](./预览/user.png)
+![用户配角色](./预览/userConfigRoles.png)
+
+## 依赖
+```
+- spring-boot 2.2.0.RELEASE
+- Mybatis-Plus 3.2.0
+- spring-security
+- java-jwt
+- hutool-all 5.0.6
+- Ant-Design-Pro-Vue
+```
+
+## 前台项目地址（基于 Ant Design Pro Vue)  
+[https://github.com/qinyou/api-server-client](https://github.com/qinyou/api-server-client)
--- src/main/java/com/qinyou/apiserver/sys/controller/LogController.java
+++ src/main/java/com/qinyou/apiserver/sys/controller/LogController.java
@@ -1,8 +1,23 @@
 package com.qinyou.apiserver.sys.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.qinyou.apiserver.core.aop.SysLog;
+import com.qinyou.apiserver.core.base.PageDTO;
+import com.qinyou.apiserver.core.base.PageFindDTO;
+import com.qinyou.apiserver.core.result.ResponseEnum;
+import com.qinyou.apiserver.core.result.ResponseResult;
+import com.qinyou.apiserver.core.utils.WebUtils;
+import com.qinyou.apiserver.sys.entity.Log;
+import com.qinyou.apiserver.sys.service.ILogService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,8 +27,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @author chuang
  * @since 2019-12-15
  */
+@Api(tags = "操作日志")
 @RestController
 @RequestMapping("/sys/log")
 public class LogController {
 
+    @Autowired
+    ILogService logService;
+
+    @ApiOperation("查询列表,带分页")
+    @PreAuthorize("hasAuthority('sysLog')")
+    @PostMapping("/list")
+    public ResponseResult<PageDTO<Log>> list(@RequestBody PageFindDTO pageFindDto) {
+        QueryWrapper<Log> queryWrapper = WebUtils.buildSearchQueryWrapper(pageFindDto);
+        IPage<Log> page = WebUtils.buildSearchPage(pageFindDto);
+        PageDTO<Log> pageDTO = WebUtils.buildResultPage(logService.page(page, queryWrapper));
+        return WebUtils.ok(pageDTO);
+    }
+
+
+    @ApiOperation(value = "批量删除操作日志")
+    @SysLog()
+    @PreAuthorize("hasAuthority('sysLog:remove')")
+    @PostMapping("/batch-remove")
+    public ResponseResult remove(@RequestBody  List<String> ids) {
+         logService.removeByIds(ids);
+         return WebUtils.ok(ResponseEnum.DELETE_SUCCESS);
+    }
 }
